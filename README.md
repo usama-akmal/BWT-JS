43b7dedd3ee832b7a4e929ffdf03d1777ee4a38d
UAkmal <uakmal@NIS-PK313.lan>
2025-01-27 12:07:47 +0500

    CLASS-1-JS

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # BWT-JS
+
+[Namste Javascript](https://www.youtube.com/watch?v=pN6jk0uUrD8&list=PLlasXeu85E9cQ32gLCvAvr9vNaUccPVNP)
---
+++ js/class-1.js
@@ -0,0 +1,46 @@
+var num1 = 2;
+var num2 = 4;
+var x;
+
+var sum = num1 + num2;
+console.log("Sum:", sum);
+
+var multiplication = num1 * num2;
+var multiplicationResult = "multiplication: \n\n" + multiplication;
+console.log(multiplicationResult);
+
+var power = num1 ** num2;
+console.log(`Power: ${power}`)
+
+const test = 1;
+console.log("test:", test);
+
+
+const human = {
+    firstName: "Usama"
+}
+
+console.log("HUman: ", human);
+
+human["age"] = 28;
+
+console.log("HUman: ", human);
+
+const arr = [1,2,3,4,5];
+console.log("arr:", arr);
+
+arr.push(6);    
+console.log("arr:", arr);
+
+console.log("value at index 3: ", arr[3]);
+
+console.log("value at key firstName: ", human["firstName1"]);
+
+console.log("x", x);
+
+x = 2;
+
+const trueValue = true;
+const falseValue = false;
+
+console.log(trueValue, falseValue, trueValue === '1');
