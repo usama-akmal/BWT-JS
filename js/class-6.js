968e738dce3d8821cbb6043997ba0398659715de
Usama Akmal <iusamaakmal@gmail.com>
2025-02-19 10:20:56 +0500

    Class-6

---
+++ js/class-6.js
@@ -0,0 +1,34 @@
+// // Shallow Copy
+// const arr = [1,2,3,4,5];
+// const narr = arr;
+// narr[0] = 6;
+
+// console.log("arr", arr);
+// console.log("narr", narr);
+
+// // Deep Copy
+
+// const darr = []
+// for (let index = 0; index < arr.length; index++) {
+//     const element = arr[index];
+//     darr.push(element);
+// }
+// console.log("darr", darr);
+// darr[2] = 8;
+
+// console.log("arr", arr);
+// console.log("narr", narr);
+// console.log("darr", darr);
+
+
+// Question 3
+// const arr = [1,2,34,1,2,3,4,5,6,1,2,23,4,4,5]
+// const unique = []; // [1, 2, 34, 3, 4, 5, 6, 23]
+
+// for (let i = 0; i < arr.length; i++) {
+//     const element = arr[i];
+//     if (unique.includes(element) === false) {
+//         unique.push(element);
+//     }   
+// }
+
